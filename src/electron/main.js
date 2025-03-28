05b877a2c7b2fe31ddf3c6e3c4d135fba6b1691e
unknown <ezekuiel100@gmail.com>
2025-03-28 15:17:16 -0300

    modern implementation

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,7 +1,18 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
 
-app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
-  mainWindow.loadFile(path.join(app.getAppPath() + "/dist-react/index.html"));
+function createWindow() {
+  const win = new BrowserWindow({});
+
+  win.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
