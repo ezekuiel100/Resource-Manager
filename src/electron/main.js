ed2509dee7eaeb88cc4a844d7f1f951971232382
unknown <ezekuiel100@gmail.com>
2025-03-30 11:00:32 -0300

    handle static data

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,18 +1,27 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, ipcMain } from "electron";
 import path from "path";
-import { resources } from "./resourceManager.js";
-
-//cpu, ram, storage
+import { getStaticData, resources } from "./resourceManager.js";
 
 function createWindow() {
-  const win = new BrowserWindow({});
+  const win = new BrowserWindow({
+    webPreferences: {
+      preload: path.join(app.getAppPath(), "src/electron/preload.js"),
+      contextIsolation: true,
+      enableRemoteModule: false,
+    },
+  });
 
-  resources();
   if (process.env.NODE_ENV === "development") {
     win.loadURL("http://localhost:5123");
+    console.log("Janela carregada com sucesso ");
   } else {
     win.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
   }
+
+  resources(win);
+  ipcMain.handle("static-data", () => {
+    return getStaticData();
+  });
 }
 
 app.whenReady().then(() => {
