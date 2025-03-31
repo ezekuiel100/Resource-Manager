494f7bad1348a3dda6fbd884846f285eb88f1c1b
unknown <ezekuiel100@gmail.com>
2025-03-31 13:23:01 -0300

    ram and storage graf

--- src/ui/App.jsx
+++ src/ui/App.jsx
@@ -1,69 +1,81 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import {
-  Area,
-  AreaChart,
-  CartesianGrid,
-  Line,
-  LineChart,
-  ResponsiveContainer,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from "recharts";
+import { Area, AreaChart, CartesianGrid, XAxis, YAxis } from "recharts";
+import { useStatistic } from "./useStatistic";
 
 function App() {
-  const [cpu, setCpu] = useState([]);
-  const [ram, setRam] = useState(0);
-  const [storage, setStorage] = useState(0);
   const [staticData, setStaticData] = useState();
+  const { cpuData, ramData, sotageData } = useStatistic();
 
   useEffect(() => {
-    window.electron.getData((data) => {
-      setCpu((prev) => {
-        const newData = [...prev, data.cpu];
-
-        if (newData.length > 10) {
-          newData.shift();
-        }
-
-        return newData;
-      });
-    });
     window.electron.getStaticData().then((data) => {
       setStaticData(data);
     });
   }, []);
 
-  const preparedCpuData = cpu.map((value) => ({ value }));
-
   return (
     <main>
-      <p>
-        <span>CPU</span> {staticData?.cpuModel}
-      </p>
+      <div>
+        <p>
+          <span>CPU</span> {staticData?.cpuModel}
+        </p>
+        <AreaChart width={300} height={100} data={cpuData}>
+          <CartesianGrid stroke="#333" strokeDasharray="5 5" fill="#1C1C1C" />
+          <Area
+            fillOpacity={0.3}
+            fill="#8884d8"
+            stroke="#8884d8"
+            strokeWidth={3}
+            type="monotone"
+            dataKey="value"
+            isAnimationActive={false}
+          />
+          <XAxis stroke="transparent" height={0} />
+          <YAxis domain={[0, 100]} stroke="transparent" width={0} />
+        </AreaChart>
+      </div>
 
-      <AreaChart width={300} height={100} data={preparedCpuData}>
-        <CartesianGrid stroke="#333" strokeDasharray="5 5" fill="#1C1C1C" />
-        <Area
-          fillOpacity={0.3}
-          fill="#8884d8"
-          stroke="#8884d8"
-          strokeWidth={3}
-          type="monotone"
-          dataKey="value"
-          isAnimationActive={false}
-        />
-        <XAxis stroke="transparent" height={0} />
-        <YAxis domain={[0, 100]} stroke="transparent" width={0} />
-      </AreaChart>
+      <div>
+        <p>
+          <span>RAM</span> {staticData?.totalMemoryGB} GB
+        </p>
 
-      <p>
-        <span>RAM</span> {staticData?.totalMemoryGB} GB
-      </p>
-      <p>
-        <span>STORAGE</span> {staticData?.totalStorage.toFixed()} GB
-      </p>
+        <AreaChart width={300} height={100} data={ramData}>
+          <CartesianGrid stroke="#333" strokeDasharray="5 5" fill="#1C1C1C" />
+          <Area
+            fillOpacity={0.3}
+            fill="#8884d8"
+            stroke="#8884d8"
+            strokeWidth={3}
+            type="monotone"
+            dataKey="value"
+            isAnimationActive={false}
+          />
+          <XAxis stroke="transparent" height={0} />
+          <YAxis domain={[0, 100]} stroke="transparent" width={0} />
+        </AreaChart>
+      </div>
+
+      <div>
+        <p>
+          <span>STORAGE</span> {staticData?.totalStorage.toFixed()} GB
+        </p>
+
+        <AreaChart width={300} height={100} data={sotageData}>
+          <CartesianGrid stroke="#333" strokeDasharray="5 5" fill="#1C1C1C" />
+          <Area
+            fillOpacity={0.3}
+            fill="#8884d8"
+            stroke="#8884d8"
+            strokeWidth={3}
+            type="monotone"
+            dataKey="value"
+            isAnimationActive={false}
+          />
+          <XAxis stroke="transparent" height={0} />
+          <YAxis domain={[0, 1000]} stroke="transparent" width={0} />
+        </AreaChart>
+      </div>
     </main>
   );
 }
